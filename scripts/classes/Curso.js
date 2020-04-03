a8d75b6518b1e905c4813173fc831e96646f1e06
Fernando Reyes <ferchoreya@gmail.com>
2020-04-02 23:36:32 -0600

    Clase Curso.js
    Se crea la primera clase Curso.js

---
+++ scripts/classes/Curso.js
@@ -0,0 +1,33 @@
+//Le indicamos que la clase Curso, sea una clase exportada (export default)
+export default class Curso {
+    constructor(nombre,poster,clases) {
+        this.nombre = nombre
+        this.poster = poster
+        this.clases = clases
+        this.inscritos = []
+    }
+    //Getters y setters
+    setNombre(nombre) {
+        this.nombre = nombre
+    }
+
+    getNombre() {
+        return this.nombre
+    }
+
+    setPoster(poster) {
+        this.poster = poster
+    }
+
+    getPoster() {
+        return this.poster
+    }
+
+    setClases(clases) {
+        this.clases = clases
+    }
+
+    getClases() {
+        return this.clases
+    }
+}
--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,11 @@
-alert("Hola!!")
+//Para hacer uso de una clase exportada, en este archivo la vamos a importar
+import Curso from "./classes/Curso.js"
+/* 
+NOTA: No basta con tener una clase exportada e importarla donde se va a utilizar,
+ya que esto provoca el siguiente error en el navegador: 
+Uncaught SyntaxError: Cannot use import statement outside a module
+Por lo que debemos agregar en el HTML que se utiliza como modulo
+<script type="module" src="scripts/index.js"></script>
+*/
+const html = new Curso("HTML desde cero","http://bancodeimagenes/tec/html5.png",10)
+console.log(html)
